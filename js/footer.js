3798dbcbf144e89fd93abef71bbc32a8c5095e0d
Moses Johansson <mosesjohansson@gmail.com>
2016-12-06 10:57:10 +0100

    footer file rename

--- js/footer.js
+++ js/footer.js
@@ -1,32 +1,32 @@
-﻿document.write('<div class="footer">'+
-        '<footer>' +
-            '<div class="container">' +
-               ' <div class="row"> ' +
-                   ' <div class="col-md-6 col-md-push-6 item text-right">' +
-                        '<h4>ComputerShop Inc.</h4>' +
-                       '<p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>'+
-                    '</div>' +
-                    '<div class="col-md-3 col-md-pull-6 col-sm-4 item text-right" style="list-style-type: none;">' +
-                     '<h4>Services</h4>' +
-
-                       '<li><a href="#">Sales</a></li>' +
-                       '<li><a href="#">Delivery</a></li>' +
-                        '<li><a href="#">Repair</a></li>' +
-
-                    '</div>' +
-                    '<div class="col-md-3 col-md-pull-6 col-sm-4 item text-right" style="list-style-type: none;">' +
-                     '<h4>About</h4>' +
-
-                       '<li><a href="#">Company</a></li>' +
-                       '<li><a href="#">News</a></li>' +
-                       '<li><a href="#">Careers</a></li>' +
-
-                       '<br>' +
-                   '</div>' +
-
-                    '<div class="col-md-12 col-sm-4 item text-right"><a href="#"><i class="fa fa-twitter-square" style="font-size: 24px;" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-facebook-official" style="font-size: 24px;" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-snapchat-square" style="font-size: 24px;" aria-hidden="true"></i></a> <a href="#"><i class="fa fa-instagram" style="font-size: 24px;" aria-hidden="true"></i></a></div>' +
-                '</div>' +
-               '<p class="copyright text-right">ComputerShop Inc.© 2016</p>' +
-            '</div>' +
-        '</footer>' +
-   '</div>');
+﻿document.writeln("         <!--Footer--> "); 
+document.writeln("         <div class=\"footer\"> "); 
+document.writeln("             <footer> "); 
+document.writeln("                 <div class=\"container\"> "); 
+document.writeln("                     <div class=\"row\"> "); 
+document.writeln("                         <div class=\"col-md-6 col-md-push-6 item text\"> "); 
+document.writeln("                             <h3>Company Name</h3> "); 
+document.writeln("                             <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p> "); 
+document.writeln("                         </div> "); 
+document.writeln("                         <div class=\"col-md-3 col-md-pull-6 col-sm-4 item\"> "); 
+document.writeln("                             <h3>Services</h3> "); 
+document.writeln("                             <ul> "); 
+document.writeln("                                 <li><a href=\"#\">Web design</a></li> "); 
+document.writeln("                                 <li><a href=\"#\">Development</a></li> "); 
+document.writeln("                                 <li><a href=\"#\">Hosting</a></li> "); 
+document.writeln("                             </ul> "); 
+document.writeln("                         </div> "); 
+document.writeln("                         <div class=\"col-md-3 col-md-pull-6 col-sm-4 item\"> "); 
+document.writeln("                             <h3>About</h3> "); 
+document.writeln("                             <ul> "); 
+document.writeln("                                 <li><a href=\"#\">Company</a></li> "); 
+document.writeln("                                 <li><a href=\"#\">Team</a></li> "); 
+document.writeln("                                 <li><a href=\"#\">Careers</a></li> "); 
+document.writeln("                             </ul> "); 
+document.writeln("                         </div> "); 
+document.writeln("                         <div class=\"col-md-12 col-sm-4 item social\"><a href=\"#\"><i class=\"fa fa-twitter-square\" style=\"font-size: 24px;\" aria-hidden=\"true\"></i></a> <a href=\"#\"><i class=\"fa fa-facebook-official\" style=\"font-size: 24px;\" aria-hidden=\"true\"></i></a> <a href=\"#\"><i class=\"fa fa-snapchat-square\" style=\"font-size: 24px;\" aria-hidden=\"true\"></i></a> <a href=\"#\"><i class=\"fa fa-instagram\" style=\"font-size: 24px;\" aria-hidden=\"true\"></i></a></div> "); 
+document.writeln("                     </div> "); 
+document.writeln("                     <p class=\"copyright\">Company Name © 2016</p> "); 
+document.writeln("                 </div> "); 
+document.writeln("             </footer> "); 
+document.writeln("         </div> ");
+// -->
