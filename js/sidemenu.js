56d38b654fd25502708779c33426ab817e42c06f
Moses Johansson <mosesjohansson@gmail.com>
2016-12-07 12:45:29 +0100

    css fonts..

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -2,11 +2,6 @@
 document.writeln(" 					<ul class=\"nav nav-pills nav-stacked affix-top\" data-spy=\"affix\" data-offset-top=\"205\"> "); 
 document.writeln(" 						<li class=\"dropdown active\"> "); 
 document.writeln(" 							<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\" aria-expanded=\"false\">Products <span class=\"caret\"></span></a> "); 
-document.writeln("<li class=\"mobile-view-li\"><a href=\"search.html\">Search</a></li> ");
-document.writeln("<li class=\"mobile-view-li\"><a href=\"cart.html\">Cart</a></li> ");
-    
-
-
 document.writeln(" 							<ul class=\"dropdown-menu\"> ");
 document.writeln(" 								<li><a href=\"laptop.html\">Laptops</a></li> "); 
 document.writeln(" 								<li><a href=\"printer.html\">Printers</a></li> "); 
